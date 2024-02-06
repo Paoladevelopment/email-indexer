62f389c5a69fa541fa423fc8e243b40107bec9fb
Paoladevelopment <paolala1017@gmail.com>
2024-02-05 23:46:08 -0500

    Adding a dockercompose file for starting a zincsearch service if it is not installed inthe pc locally

--- enron-indexer/env/loadEnvVars.go
+++ enron-indexer/env/loadEnvVars.go
@@ -1,10 +1,7 @@
 package env
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 type envConfig struct {
@@ -16,12 +13,6 @@ type envConfig struct {
 var EnvVars envConfig
 
 func loadEnvVars() envConfig {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return envConfig{
 		ZincUser: os.Getenv("ZINC_FIRST_ADMIN_USER"),
 		ZincPass: os.Getenv("ZINC_FIRST_ADMIN_PASSWORD"),
--- enron-indexer/go.mod
+++ enron-indexer/go.mod
@@ -1,5 +1,3 @@
 module github.com/Paoladevelopment/enron-indexer
 
 go 1.21.5
-
-require github.com/joho/godotenv v1.5.1
