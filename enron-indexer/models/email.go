0cdc262dbfcfcfe4684f47f1fc0c74ddcd8c4f4f
Paoladevelopment <paolala1017@gmail.com>
2024-02-21 19:09:30 -0500

    deleting an attribute from the emails model that is not used

--- enron-indexer/models/email.go
+++ enron-indexer/models/email.go
@@ -1,25 +1,16 @@
 package models
 
 type Email struct {
-	MessageID string     `json:"message_id"`
-	Date      string     `json:"date"`
-	From      string     `json:"from"`
-	XFrom     string     `json:"x_from"`
-	To        string     `json:"to"`
-	XTo       string     `json:"x_to"`
-	Subject   string     `json:"subject"`
-	Cc        string     `json:"cc"`
-	XCc       string     `json:"x_cc"`
-	Bcc       string     `json:"bcc"`
-	XBcc      string     `json:"x_bcc"`
-	Content   string     `json:"content"`
-	Subemails []SubEmail `json:"subemails"`
-}
-
-type SubEmail struct {
-	From    string `json:"from"`
-	Sent    string `json:"sent"`
-	To      string `json:"to"`
-	Subject string `json:"subject"`
-	Content string `json:"content"`
+	MessageID string `json:"message_id"`
+	Date      string `json:"date"`
+	From      string `json:"from"`
+	XFrom     string `json:"x_from"`
+	To        string `json:"to"`
+	XTo       string `json:"x_to"`
+	Subject   string `json:"subject"`
+	Cc        string `json:"cc"`
+	XCc       string `json:"x_cc"`
+	Bcc       string `json:"bcc"`
+	XBcc      string `json:"x_bcc"`
+	Content   string `json:"content"`
 }
